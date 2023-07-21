7c747fda43d8f502168aad37fffbb2cdf7c8695f
Murillo_Zubk <mzubk.dev@gmail.com>
2023-07-21 15:21:37 -0300

    adicionando a async function buscaEndereco CEP

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
-var consultaCEP = fetch("https://viacep.com.br/ws/01001000/json/");
-
-console.log(consultaCEP);
+async function buscaEndereco(cep) {
+  try {
+    var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+    var consultaCEPConvertida = await consultaCEP.json();
+    if (consultaCEPConvertida.erro) {
+      throw Error("CEP não existente!");
+    }
+    console.log(consultaCEPConvertida);
+    return consultaCEPConvertida;
+  } catch (erro) {
+    console.log(erro);
+  }
+}
